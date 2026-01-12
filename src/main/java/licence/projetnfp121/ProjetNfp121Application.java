32bf014b56cdb995a86d4183b41b6dc455c54306
arthu <arthur30dub@gmail.com>
2026-01-12 10:04:59 +0100

    first

---
+++ src/main/java/licence/projetnfp121/ProjetNfp121Application.java
@@ -0,0 +1,13 @@
+package licence.projetnfp121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNfp121Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetNfp121Application.class, args);
+    }
+
+}
---
+++ src/test/java/licence/projetnfp121/ProjetNfp121ApplicationTests.java
@@ -0,0 +1,13 @@
+package licence.projetnfp121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetNfp121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
